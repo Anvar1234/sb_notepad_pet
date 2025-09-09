a4cf97c7c8cd60322f2b5afb4f2d4e41ef698e27
Anvar1234 <kingartaved@yandex.ru>
2025-09-10 00:38:13 +0500

    feat: добавлен функционал в сервисе

--- src/main/java/ru/yandex/kingartaved/sb_notepad/service/NoteServiceImpl.java
+++ src/main/java/ru/yandex/kingartaved/sb_notepad/service/NoteServiceImpl.java
@@ -8,10 +8,41 @@ import ru.yandex.kingartaved.sb_notepad.data.model.Note;
 import ru.yandex.kingartaved.sb_notepad.data.model.TextContent;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class NoteServiceImpl implements NoteService {
+
+    private final Map<Long, Note> noteMap = Map.of(
+            1L, Note.builder()
+                    .id(1L)
+                    .title("Note1 title")
+                    .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
+                    .pinned(false)
+                    .type(NoteTypeEnum.TEXT_NOTE)
+                    .content(new TextContent("Text1 note body"))
+                    .build(),
+            2L, Note.builder()
+                    .id(2L)
+                    .title("Checklist2 title")
+                    .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
+                    .pinned(false)
+                    .type(NoteTypeEnum.CHECKLIST)
+                    .content(new ChecklistContent(
+                            List.of(new ChecklistTask("Task1", false),
+                                    new ChecklistTask("Task2", false))))
+                    .build(),
+            3L, Note.builder()
+                    .id(3L)
+                    .title("Название3")
+                    .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
+                    .pinned(false)
+                    .type(NoteTypeEnum.TEXT_NOTE)
+                    .content(new TextContent("TextNote3 new content"))
+                    .build()
+    );
 
     @Override
     public Note createNote(Note note) {
@@ -20,36 +51,10 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public Note getNoteById(Long id) {
-        return Note.builder()
-                .id(1L)
-                .title("Note title")
-                .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
-                .pinned(false)
-                .type(NoteTypeEnum.TEXT_NOTE)
-                .content(new TextContent("TextNote new content"))
-                .build();
+        return noteMap.get(id);
     }
 
     public List<Note> getAllNotes() {
-        return List.of(
-                Note.builder()
-                        .id(1L)
-                        .title("Text note title")
-                        .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
-                        .pinned(false)
-                        .type(NoteTypeEnum.TEXT_NOTE)
-                        .content(new TextContent("TextNote new content"))
-                        .build(),
-                Note.builder()
-                        .id(2L)
-                        .title("Checklist title")
-                        .createdAt(LocalDateTime.of(2023, 5, 24, 0, 0))
-                        .pinned(false)
-                        .type(NoteTypeEnum.CHECKLIST)
-                        .content(new ChecklistContent(
-                                List.of(new ChecklistTask("Task1", false),
-                                        new ChecklistTask("Task2", false))))
-                        .build()
-        );
+        return noteMap.values().stream().toList();
     }
 }
